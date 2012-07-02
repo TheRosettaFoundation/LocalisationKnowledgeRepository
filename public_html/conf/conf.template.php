8b2ff3d4793759a875dd8c9219c2fcb441879e63
spaceindaver <spaceindaver0@gmail.com>
2012-07-02 13:28:55 +0100

    Update the conf template with new values

--- public_html/conf/conf.template.php
+++ public_html/conf/conf.template.php
@@ -23,4 +23,13 @@ $files['segmenter'] = $_SERVER['DOCUMENT_ROOT'].'/segment-java/bin/segment';
 $files['srx'] = $_SERVER['DOCUMENT_ROOT'].'/segment-java/example/simple.srx';
 $files['language'] = 'english';
 
+/*
+    $domain['root'] is the directory in which the lkr is installed
+    e.g. for path /opt/lampp/htdocs/lkr/index.php $domain['root'] = 'lkr'
+    No leading or trailing slashes are required
+    If installed in the web server root just leave blank
+*/
+$domain['root'] = 'root_dir';
+$cnlf['url'] = 'http://193.1.97.50/locconnect/';
+
 ?>
